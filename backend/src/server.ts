d254108dc1b52009b55cb422435114085df3437f
Ritesh Khore <riteshkhore@gmail.com>
2022-03-13 14:19:58 +0530

    Added json limit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -30,7 +30,7 @@ app.use(morgan("dev"));
 
 app.use(helmet());
 
-app.use(express.json());
+app.use(express.json({limit:"100mb"}));
 
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
